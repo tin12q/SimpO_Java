554893da56842f38f752eaa2256f8e65d4cca9ce
Thịnh Nguyễn <thinhnp.22git@vku.udn.vn>
2022-12-13 22:21:04 +0700

    Add mainServer

--- src/main/java/com/httt/simpo_java/view/mainServer.java
+++ src/main/java/com/httt/simpo_java/view/mainServer.java
@@ -1,5 +1,25 @@
 package com.httt.simpo_java.view;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class mainServer {
+
+    @FXML
+    private Button lamDe;
+
+    @FXML
+    private Button server;
+
+    @FXML
+    void lamDeClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void serverClick(ActionEvent event) {
+
+    }
 
 }
