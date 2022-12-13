ef8dff7674278f0a476088476bc4c2fe77944ffe
vkutin12q <thinhnp.22git@vku.udn.vn>
2022-12-13 17:25:02 +0700

    init

---
+++ src/main/java/com/httt/simpo_java/HelloController.java
@@ -0,0 +1,14 @@
+package com.httt.simpo_java;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.httt.simpo_java {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.httt.simpo_java to javafx.fxml;
+    exports com.httt.simpo_java;
+}
