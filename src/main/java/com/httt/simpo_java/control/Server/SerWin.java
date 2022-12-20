2be6ab9ada14b653cfe2d7aba94d4a1b128a5bb9
vkutin12q <thinhnp.22git@vku.udn.vn>
2022-12-20 15:51:55 +0700

    Update UI Server

--- src/main/java/com/httt/simpo_java/control/Server/SerWin.java
+++ src/main/java/com/httt/simpo_java/control/Server/SerWin.java
@@ -67,19 +67,29 @@ public class SerWin {
     private void loadPage(String name,ActionEvent event) {
 
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/httt/simpo_java/Server/SerWinPane/"+name + ".fxml"));
-
-            AnchorPane newPane = loader.load();
-
-            // Set the loaded FXML file as the content of our main right-side pane
-            ac1.getChildren().setAll(newPane);
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            stage.setTitle(name);
-            // Reset the anchors
-            AnchorPane.setBottomAnchor(newPane, 0.0);
-            AnchorPane.setLeftAnchor(newPane, 0.0);
-            AnchorPane.setRightAnchor(newPane, 0.0);
-            AnchorPane.setTopAnchor(newPane, 0.0);
+            if(stage.getTitle().equals(name))
+            {
+                return;
+            }
+            else {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/httt/simpo_java/Server/SerWinPane/"+name + ".fxml"));
+
+                AnchorPane newPane = loader.load();
+
+                // Set the loaded FXML file as the content of our main right-side pane
+                ac1.getChildren().setAll(newPane);
+
+                stage.setTitle(name);
+                // Reset the anchors
+                AnchorPane.setBottomAnchor(newPane, 0.0);
+                AnchorPane.setLeftAnchor(newPane, 0.0);
+                AnchorPane.setRightAnchor(newPane, 0.0);
+                AnchorPane.setTopAnchor(newPane, 0.0);
+                System.out.println("Loaded " + name + " page");
+                System.out.println(stage.getTitle());
+            }
+
 
         } catch (IOException e) {
             e.printStackTrace();
