fd6c8bf3cad94f43b1956b45602a65cede0a01b8
Le Gia Bao <lgbao1062003@gmail.com>
2023-03-07 23:16:39 +0800

    Initialize players' in4

--- src/main/java/com/httt/server/control/Server/SerWinPane/obstacleRound.java
+++ src/main/java/com/httt/server/control/Server/SerWinPane/obstacleRound.java
@@ -13,11 +13,9 @@ public class obstacleRound extends updateInformation{
     @FXML private Label nameOfS1, nameOfS2, nameOfS3, nameOfS4;
     @FXML private Label nameOfS11, nameOfS22, nameOfS33, nameOfS44;
     @FXML private Label scoreOfS1, scoreOfS2, scoreOfS3, scoreOfS4;
-
-    @FXML private void smallConfirmBtn(ActionEvent event) throws IOException {
+    @FXML private void initialize() {
         super.setName(nameOfS1, nameOfS2, nameOfS3, nameOfS4);
         super.setName(nameOfS11, nameOfS22, nameOfS33, nameOfS44);
         super.setScores(scoreOfS1, scoreOfS2, scoreOfS3, scoreOfS4);
     }
-
 }
