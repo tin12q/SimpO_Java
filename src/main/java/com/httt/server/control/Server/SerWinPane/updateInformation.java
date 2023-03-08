5012b31b8d01aaf57a0d0cd4c112f8babaf884dd
BaoLe106 <127121163+BaoLe106@users.noreply.github.com>
2023-03-08 11:19:22 +0700

    Update updateInformation.java

--- src/main/java/com/httt/server/control/Server/SerWinPane/updateInformation.java
+++ src/main/java/com/httt/server/control/Server/SerWinPane/updateInformation.java
@@ -1,44 +1,65 @@
-package com.httt.server.control.Server.SerWinPane;
+package com.httt.server;
 
-import io.github.palexdev.materialfx.controls.MFXTextField;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class updateInformation {
-    Participants participants = new Participants();
-    public void setName(Label nameOfS1, Label nameOfS2, Label nameOfS3, Label nameOfS4) {
-        nameOfS1.setText(participants.getName(0)); nameOfS2.setText(participants.getName(1));
-        nameOfS3.setText(participants.getName(2)); nameOfS4.setText(participants.getName(3));
+public class Main extends Application {
+    @FXML
+    private MFXButton lamDe;
+
+    @FXML
+    private MFXButton server;
+    @FXML
+    void lamDeClick(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/com/httt/server/ExamMaker/examMaker.fxml"));
+        Parent content = loader.load();
+
+        Scene scene = new Scene(content);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.setX(0); stage.setY(0);
+        stage.setTitle("Exam Maker");
+        stage.show();
+    }
+    @FXML
+    void serverClick(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/com/httt/server/Server/SerWin.fxml"));
+        Parent content = loader.load();
+
+        Scene scene = new Scene(content);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.setX(0); stage.setY(0);
+        stage.setTitle("Server");
+        stage.show();
     }
 
-    public void setName(MFXTextField nameOfS1, MFXTextField nameOfS2, MFXTextField nameOfS3, MFXTextField nameOfS4) {
-        nameOfS1.setText(participants.getName(0)); nameOfS2.setText(participants.getName(1));
-        nameOfS3.setText(participants.getName(2)); nameOfS4.setText(participants.getName(3));
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("mainServer.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Main");
+        stage.sizeToScene();
+        stage.show();
     }
 
-    public void setScores(Label scoreOfS1, Label scoreOfS2, Label scoreOfS3, Label scoreOfS4) {
-        scoreOfS1.setText(String.valueOf(participants.getScore(0)));
-        scoreOfS2.setText(String.valueOf(participants.getScore(1)));
-        scoreOfS3.setText(String.valueOf(participants.getScore(2)));
-        scoreOfS4.setText(String.valueOf(participants.getScore(3)));
-    }
-
-    public void setScores(MFXTextField scoreOfS1, MFXTextField scoreOfS2, MFXTextField scoreOfS3, MFXTextField scoreOfS4) {
-        scoreOfS1.setText(String.valueOf(participants.getScore(0)));
-        scoreOfS2.setText(String.valueOf(participants.getScore(1)));
-        scoreOfS3.setText(String.valueOf(participants.getScore(2)));
-        scoreOfS4.setText(String.valueOf(participants.getScore(3)));
-    }
-
-    public void updateScore(int index, Label scoreOfS1, Label scoreOfS2, Label scoreOfS3, Label scoreOfS4) {
-        switch (index) {
-            case 0: scoreOfS1.setText(String.valueOf(participants.getScore(0)));
-            case 1: scoreOfS2.setText(String.valueOf(participants.getScore(1)));
-            case 2: scoreOfS3.setText(String.valueOf(participants.getScore(2)));
-            case 3: scoreOfS4.setText(String.valueOf(participants.getScore(3)));
-        }
+    public static void main(String[] args) {
+        launch();
     }
 }
