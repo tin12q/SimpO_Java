316a7b90d9fa6d8555fdc7ae5c2aa42c66682794
Thịnh Nguyễn <120249315+vkutin12q@users.noreply.github.com>
2023-03-25 19:06:39 +0700

    Update obstacle

--- Client/src/main/java/com/httt/client/control/clientWin/Pane/obstacle.java
+++ Client/src/main/java/com/httt/client/control/clientWin/Pane/obstacle.java
@@ -15,7 +15,7 @@ public class obstacle {
     @FXML private Label questionLb;
     @FXML private ImageView imageView;
     @FXML private AnchorPane obstaclePane;
-    private final VBox questionVb = new VBox();
+    private final VBox answerVb = new VBox();
     private Rectangle[] rec = new Rectangle[5];
     private static Question[] questionList = new Question[4];
     @FXML private void initialize() {
@@ -40,17 +40,18 @@ public class obstacle {
         Image img = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Letter_a.svg/997px-Letter_a.svg.png");
         imageView.setImage(img);
         //question list
-        obstaclePane.getChildren().add(questionVb);
-        questionVb.setPrefHeight(300); questionVb.setPrefWidth(450);
-        questionVb.setSpacing(10);
-        questionVb.setAlignment(Pos.CENTER);
-        AnchorPane.setTopAnchor(questionVb,180.0);
-        AnchorPane.setLeftAnchor(questionVb,233.0+225+30);
-        AnchorPane.setRightAnchor(questionVb,233.0+225+30);
+        obstaclePane.getChildren().add(answerVb);
+        answerVb.setPrefHeight(300); answerVb.setPrefWidth(450);
+        answerVb.setSpacing(10);
+        answerVb.setAlignment(Pos.CENTER);
+        AnchorPane.setTopAnchor(answerVb,180.0);
+        AnchorPane.setLeftAnchor(answerVb,233.0+225+30);
+        AnchorPane.setRightAnchor(answerVb,233.0+225+30);
         for(int i=0;i<4;i++){
             questionList[i]= new Question("Question " + (i + 1), "Answer " + (i + 1));
-            questionVb.getChildren().add(questionList[i].getQuestion());
+            answerVb.getChildren().add(questionList[i].getAnswer());
         }
+        questionLb.setText("Question: " + questionList[0].question);
     }
 
     static class Question {
@@ -60,17 +61,18 @@ public class obstacle {
             this.question = question;
             this.answer = answer;
         }
-        public HBox getQuestion(){
-            HBox questionHb = new HBox();
-            questionHb.setSpacing(10);
-            for (int i = 0; i < question.length(); i++) {
-                Label lb = new Label(question.charAt(i) + "");
+        public HBox getAnswer(){
+            HBox answerHb = new HBox();
+            answerHb.setSpacing(10);
+            answerHb.setAlignment(Pos.CENTER);
+            for (int i = 0; i < answer.length(); i++) {
+                Label lb = new Label(answer.charAt(i) + "");
                 lb.setPrefSize(30, 30);
                 lb.setAlignment(Pos.CENTER);
                 lb.setStyle("-fx-font-size: 20px;"+"-fx-background-color: #FBFBF2;"+"-fx-text-fill: #847577;"+"-fx-border-radius: 50%;"+"-fx-background-radius: 50%;");
-                questionHb.getChildren().add(lb);
+                answerHb.getChildren().add(lb);
             }
-            return questionHb;
+            return answerHb;
         }
     }
 }
